9bb6c88717d13302916f32b95696db662ef3145f
Saba Shagidze <sabadoeswebdev@gmail.com>
2026-07-13 03:09:53 +0400

    feat: error pages

--- foodie/urls.py
+++ foodie/urls.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.shortcuts import render
+
+# Error handlers
+def handler404(request, exception):
+    return render(request, '404.html', status=404)
+
+def handler500(request):
+    return render(request, '500.html', status=500)
+
+def handler403(request, exception):
+    return render(request, '403.html', status=403)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.recipes.urls')),  # This includes the recipes URLs
+    path('', include('apps.recipes.urls')),
     path('accounts/', include('apps.accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
